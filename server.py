dba845d37891d933c16ed9dbe1d41f46fc6ae0d3
Ved Dandekar <veddandekar6@gmail.com>
2019-09-06 12:57:12 +0530

    PWD, CWD, LIST basic implementation

--- server.py
+++ server.py
@@ -2,23 +2,24 @@ import socket
 import threading
 import os
 
-#pwd
+# pwd
 dirpath = os.getcwd()
 print("current directory is : " + dirpath)
 
-#cd pathname
+# cd pathname
 # direct = input()
 # dirpath = os.path.join(dirpath, direct)     #IMPLEMENT TRY CATCH
 
-#cd ..
-print(os.path.abspath('..'))    #LIMIT
+# cd ..
+print(os.path.abspath('..'))    # LIMIT
 
-#ls
+# ls
 print(os.listdir(dirpath))
+
 
 def server():
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serversocket.bind(("localhost", 2871))
+    serversocket.bind(("localhost", 2772))
     serversocket.listen(5)
 
     print("Waiting for client")
@@ -27,26 +28,37 @@ def server():
     return client
 
 
+def reply(msg):
+    s.send(msg.encode('ascii'))
+
+
 def chat_receive():
     while True:
         if not s:
             print("CLIENT RIP")
             return
-
+        global dirpath
         msg = s.recv(4096).decode('ascii')
 
         print(msg)
 
-        if msg == "LIST\r\n":                       #Directory and file colours
+        if msg == "LIST\r\n":                               # Directory and file colours
             reply_msg = ""
             for x in os.listdir(dirpath):
                 reply_msg = reply_msg + x + "\r\n"
             reply(reply_msg)
 
+        elif msg == "PWD\r\n":
+            reply_msg = dirpath + "\r\n"
+            reply(reply_msg)
 
-def reply(msg):
-    s.send(msg.encode('ascii'))
-
+        elif msg[:3] == "CWD":
+            arg = msg[4:].strip()
+            if arg[0] == "\\":
+                dirpath = os.chdir(arg)
+            else:
+                dirpath = os.path.join(dirpath, arg)        #Handle inexistent directories
+            reply(dirpath+"\r\n")
 
 s = server()
 

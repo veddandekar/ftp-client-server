2a9e24fec1bad5730ef2f5cf6c9ff78f84cb6aa4
Ved Dandekar <veddandekar@localhost.localdomain>
2019-09-10 23:23:32 +0530

    Client made OOP

--- client.py
+++ client.py
@@ -1,50 +1,49 @@
 import socket
 import threading
 
+class comm_sock:
+    def __init__(self, server):
+        self.end = False
+        self.rcvstatus = False
+        self.s = server
+        self.msg = ""
+        send_thread = threading.Thread(target=self.cmd_send)
+        send_thread.start()
+        rcv_thread = threading.Thread(target=self.cmd_rcv)
+        rcv_thread.start()
 
-end = False
-rcvstatus = False
-
-
-def chat_receive():
-    global rcvstatus, end
-    while not end:
-        global msg
-        msg = s.recv(4096)
-        rcvstatus = True
-        print(msg.decode("ascii"))
-
-
-def chat_send():
-    global end, rcvstate, msg
-    while not end:
-        inpt = input()
-        if inpt == "ls":
-            s.send("PASV\r\n".encode("ascii"))
-            rcvstatus = False
-            while not rcvstatus:
-                pass
-
-            if msg[:3] == "227":
-                l = msg.split("(")
-                a1, a2, a3, a4, p1, p2 = l[1].split(",")
-                print(a1, a2, a3, a4, p1, p2)
-        inpt = inpt + '\r\n'
-
-        s.send(inpt.encode("ascii"))
-        rcvstatus = False
-        if inpt == "QUIT\r\n":
-            end = True
+    def cmd_rcv(self):
+        while not self.end:
+            self.msg = self.s.recv(4096)
+            self.rcvstatus = True
+            print(self.msg.decode("ascii"))
 
 
-host = input("Enter IP: ")
-port = int(input("Enter Port: "))
+    def cmd_send(self):
+        while not self.end:
+            inpt = input()
+            if inpt == "ls":
+                self.s.send("PASV\r\n".encode("ascii"))
+                self.rcvstatus = False
+                while not self.rcvstatus:
+                    pass
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((host, port))
-send_thread = threading.Thread(target=chat_send)
-send_thread.start()
-receive_thread = threading.Thread(target=chat_receive)
-receive_thread.start()
-send_thread.join()
-s.close()
+                if self.msg[:3] == "227":
+                    l = self.msg.split("(")
+                    a1, a2, a3, a4, p1, p2 = l[1].split(",")
+                    print(a1, a2, a3, a4, p1, p2)
+            inpt = inpt + '\r\n'
+
+            self.s.send(inpt.encode("ascii"))
+            self.rcvstatus = False
+            if inpt == "QUIT\r\n":
+                self.end = True
+
+if __name__ == "__main__":
+    host = input("Enter IP: ")
+    port = int(input("Enter Port: "))
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))                                     #Error handling
+    comm_sock(s)
+    s.close()
